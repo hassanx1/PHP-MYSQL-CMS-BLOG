84e99b889b2d3df9259dd1a089f9cf3734fbb7e3
muwonge hassan <hassansaava@gmail.com>
2019-10-10 22:16:39 +0300

    added php functions to update posts, edit posts, update categories

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -7,12 +7,11 @@ if (isset($_GET['p_id'])){
 
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_get_post_id ";
 $select_posts_by_id = mysqli_query($con, $query);
 
 while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
-    $post_id = $row['post_id'];
     $post_author = $row['post_author'];
     $post_title = $row['post_title'];
     $post_category = $row['post_category_id'];
@@ -26,6 +25,49 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 }
 
+if (isset($_POST['update_post'])){
+
+    $post_author      = $_POST['post_author'];
+    $post_title       = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status      = $_POST['post_status'];
+    $post_image       = $_FILES['post_image']['name'];
+    $post_image_temp  = $_FILES['post_image']['tmp_name'];
+    $post_tags        = $_POST['post_tags'];
+    $post_content     = $_POST['post_content'];
+
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image)){
+
+        $query = "SELECT FROM posts WHERE post_id = $the_get_post_id";
+        $select_image = mysqli_query($con, $query);
+
+        while($row = mysqli_fetch_assoc($select_image)){
+
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .="post_title = '{$post_title}', ";
+    $query .="post_category_id = '{$post_category_id}', ";
+    $query .="post_date = now(), ";
+    $query .="post_author = '{$post_author}', ";
+    $query .="post_status = '{$post_status}', ";
+    $query .="post_tags = '{$post_tags}', ";
+    $query .="post_content = '{$post_content}', ";
+    $query .="post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_get_post_id} ";
+
+    $update_post = mysqli_query($con, $query);
+    confirm($update_post);
+    if (!$update_post){
+        die("QUERY FAILED " . mysqli_error($con));
+    }
+}
+
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
@@ -35,9 +77,29 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
         <input type="text" class="form-control" name="post_title" value="<?php echo  $post_title; ?>">
     </div>
 
-    <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id" value="<?php echo  $post_category; ?>">
+    <div class="form-group ">
+
+        <select name="post_category_id" id="" >
+
+            <?php
+
+            $query = "SELECT * FROM Categories";
+            $select_categories = mysqli_query($con, $query);
+
+            confirm($select_categories);
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+                echo "<option value='{$cat_id}'> {$cat_title}</option>";
+            }
+
+            ?>
+
+
+        </select>
+
     </div>
 
     <div class="form-group">
@@ -51,8 +113,9 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input type="file" class="form-control" name="post_image" value="<?php echo  $post_image; ?>">
+        <label for="post_image">Post Image</label><br>
+        <img src="../images/<?php echo $post_image; ?>" width="100">
+        <input type="file" class="form-control" name="post_image">
     </div>
 
     <div class="form-group">
@@ -62,12 +125,12 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"
-                  ><?php echo  $post_content; ?></textarea>
+        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10">
+            <?php echo  $post_content; ?></textarea>
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" class="form-control" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" class="form-control" name="update_post" value="Update Post">
     </div>
 
 
--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -16,22 +16,28 @@
                     $cat_id = $row['cat_id'];
                     $cat_title = $row['cat_title'];
                     ?>
-                    <input value="<?php if (isset($cat_title)){echo $cat_title;}?>" type="text" class="form-control">
-                <?php }  } ?>
+                    <input value="<?php if (isset($cat_title)){echo $cat_title;}?>" type="text" name = "cat_title" class="form-control">
+                <?php }  } 
+                
+                if (isset($_POST['update_category'])){
+
+                    $the_cat_title = $_POST['cat_title'];
+    
+                    $query = "UPDATE  categories SET cat_title = '{$the_cat_title}' WHERE cat_id = '{$cat_id}'";
+                    $update_query = mysqli_query($con, $query);
+                    if(!$update_query){
+    
+                        die("QUERY FAILED" . mysqli_error($con));
+                    }
+    
+                }
+                
+                
+                
+                ?>
 
             <?php
-            if (isset($_POST['update_category'])){
-
-                $the_cat_title = $_POST['cat_title'];
-
-                $query = "UPDATE  categories SET cat_title = '{$the_cat_title}' WHERE cat_id = '{$cat_id}'";
-                $update_query = mysqli_query($con, $query);
-                if(!$update_query){
-
-                    die("QUERY FAILED" . mysqli_error($con));
-                }
-
-            }
+            
 
             ?>
             <!--                                <input type="text" name="cat_title" class="form-control">-->
@@ -40,6 +46,6 @@
 
         <div class="form-group">
 
-            <input type="submit" name="update_category" value="UpdateCategory" class="btn btn-primary">
+            <input type="submit" name="update_category" value="Update Category" class="btn btn-primary">
         </div>
     </form>
